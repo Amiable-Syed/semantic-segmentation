842543d2588e8c532cc546bcb487cb8b3c72ad98
Saqib Naseeb <saqibnaseeb14@gmail.com>
2020-04-08 11:26:38 -0700

    Model, Config File , Unet code and model summary
    Saved Model with 90 validation accuracy, Unet Class Code, Model Summary graph

---
+++ ConfigFile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+
+class Config():
+  train_parent_dir="ModMonuSeg/Training/"
+  test_parent_dir="ModMonuSeg/Test/"
+  img_folder="TissueImages/"
+  gt_folder="GroundTruth/"
+  train_gt_extension='png'
+  test_gt_extension='png'
+  im_width = 256
+  im_height = 256
+  epochs=50
+  batch_size=64
+  optimizer=Adam()
+  callbacks = [
+    EarlyStopping(patience=10, verbose=1),
+    ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1),
+    ModelCheckpoint('unet-model.h5', verbose=1, save_best_only=True, save_weights_only=True)
+  ]
+
