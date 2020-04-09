fdb325962051aa87709b2c7f19e2d4eb8a6de67f
saqibnaseeb <saqibnaseeb14@gmail.com>
2020-04-09 22:49:05 +0500

    SegNEt with model and all config files

---
+++ SegNet_Config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+
+class Config():
+  train_parent_dir="ModMonuSeg/Training/"
+  test_parent_dir="ModMonuSeg/Test/"
+  img_folder="TissueImages/"
+  gt_folder="GroundTruth/"
+  train_gt_extension='png'
+  test_gt_extension='png'
+  im_width = 256
+  im_height = 256
+  epochs=100
+  batch_size=7
+  optimizer=Adam()
+  callbacks = [
+    EarlyStopping(patience=20, verbose=1),
+    ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1),
+    ModelCheckpoint('segnet-model.h5', verbose=1, save_best_only=True, save_weights_only=True)
+  ]
+
